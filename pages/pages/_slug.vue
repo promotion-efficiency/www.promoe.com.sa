821599df51774116e2884a9e6f14f31afeceab37
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2022-07-27 11:01:38 +0300

    ..

--- pages/pages/_slug.vue
+++ pages/pages/_slug.vue
@@ -7,8 +7,8 @@
         </ol>
     </nav>
     <h1 class="pb-3 border-bottom mb-3">{{ page.title }}</h1>
-    <div class="">
-        <img v-if="page.image" :src="require(`~/assets/images/pages/${page.image}`)" :alt="title" />
+    <div v-if="page.image" class="">
+        <img  :src="require(`~/assets/images/pages/${page.image}`)" :alt="title" />
     </div>
     <nuxt-content :document="page" />
   </article>
