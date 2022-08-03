ccdb4f4bc38c5dbfc4e21ffd2cdbdcf1f8e2686d
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2022-08-03 16:14:24 +0300

    ..

--- lang/en-US.js
+++ lang/en-US.js
@@ -1,7 +1,7 @@
 export default {
     solgan: 'Your Hike to PEAK!',
     title: 'Promotion Efficiency',
-    mainpage_title: 'Best Advertising Agency in Khobar',
+    mainpage_title: 'Best Advertising & Marketing Agency in Khobar',
     description: 'A platform of skilled Saudis who are equipped with ambition and creativity in the fields of marketing and advertisement, which makes them create unusual, innovative and out-of-the-box marketing solutions',
     keywords: 'promotion, efficiency, advertisement, marketing, khobar, eastern, research, design, photography, videography, hike to peak, websites, instagram marketing, snapchat marketing, field marketing, print design',
     copyrights: 'Promotion Efficiency Est. All right reserved. copyright 2021',
