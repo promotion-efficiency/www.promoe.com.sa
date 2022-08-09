8f50f13e76091398e1a0c035d037477d08295bad
Mutlaq Aldhbuiub <matulkn@gmail.com>
2022-08-09 15:28:49 +0300

    Updating the key

--- plugins/snaptr.js
+++ plugins/snaptr.js
@@ -1,1 +1,12 @@
-(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function(){a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};a.queue=[];var s='script';r=t.createElement(s);r.async=!0;r.src=n;var u=t.getElementsByTagName(s)[0];u.parentNode.insertBefore(r,u);})(window,document,'https://sc-static.net/scevent.min.js');snaptr('init', '2ebc6124-f6ff-48af-a473-ec29c8e14b65', {'user_email': 'contact@promoe.com.sa'});snaptr('track', 'PAGE_VIEW');
+(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+	{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+	a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+	r.src=n;var u=t.getElementsByTagName(s)[0];
+	u.parentNode.insertBefore(r,u);})(window,document,
+	'https://sc-static.net/scevent.min.js');
+
+	snaptr('init', '44cc5854-a33f-4fb0-a393-f3d69a387359', {
+	'user_email': 'contact@promoe.com.sa'
+	});
+
+	snaptr('track', 'PAGE_VIEW');
